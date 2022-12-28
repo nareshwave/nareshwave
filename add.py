6091de9f54033bcf031433b219972bf9b15484d4
U-NBOS\naresh <naresh@nblap400.nbos.vinisoft.com>
2022-12-28 22:12:10 +0530

    Adding the first .py file..
    Signed-off-by: U-NBOS\naresh <naresh@nblap400.nbos.vinisoft.com>

---
+++ add.py
@@ -0,0 +1,10 @@
+def add(a,b=2):
+    """
+    This method is used to add two numbers.
+    """
+    sum = 0
+    sum = a+b 
+    print("Total value calculated:- ",sum)
+
+# Calling the Add function
+add(4,10)
